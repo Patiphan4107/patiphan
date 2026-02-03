a6272d8f04c8301c0bf17e56feea09c7888b586b
Patiphan4107 <66010914107@msu.ac.th>
2026-02-03 17:29:55 +0700

    Delete h directory

--- h/connectdb.php
+++
@@ -1,9 +0,0 @@
-<?php
-					$host = "localhost";
-					$user = "root";
-					$pwd = "";
-					$db = "4107db";
-                    $conn = mysqli_connect($host,$user,$pwd,$db) or die (				"เชื่อมต่อฐานข้อมูลไม่ได้");
-					mysqli_query($conn, "SET NAMES utf8");
-					
-?>
